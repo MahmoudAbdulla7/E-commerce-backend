103fc1906dc154761d76e8d97df1d8d5fd347fca
MahmoudAbdalla7 <mahmoudapdallah629@gmail.com>
2023-10-17 11:41:31 +0300

    sec commit

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -3,11 +3,11 @@ import {fileUpload,fileValidation} from '../../utils/multer.js'
 import * as categoryController from './controller/category.js'
 import { validation } from "../../middleware/validation.js";
 import * as validators from "./category.validation.js";
-import subcategory from '../subcategory/subcategory.router.js'
+// import subcategory from '../subcategory/subcategory.router.js'
 import {auth} from "../../middleware/auth.js";
 import { endPoint } from "./category.endPoint.js";
 const router = Router()
-router.use("/:categoryId/subcategory",subcategory)
+// router.use("/:categoryId/subcategory",subcategory)
 
 router.post("/",fileUpload(fileValidation.image).single('image'),
             validation(validators.createCategory),auth(endPoint.create),
